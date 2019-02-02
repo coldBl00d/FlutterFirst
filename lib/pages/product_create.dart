279ec1026d92639e53af25fe4cf32b81e621f857
Akhil Raj <akhraj@oradev.oraclecorp.com>
2019-02-02 20:06:16 +0530

    Modal Example

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -4,6 +4,15 @@ class CreateProduct extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement buildll;
-    return Center(child:Text("Create Product"));
+    return Center(
+      child:RaisedButton(
+        child: Text("Create Product"),
+        onPressed: (){
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context){
+              return Center(child: Text("Hello there"),);
+            });
+        },));
   }
 }
