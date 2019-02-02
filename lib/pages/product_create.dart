2f822d39884714e73152c1d00c86a7dc85c6c639
Akhil Raj <akhraj@oradev.oraclecorp.com>
2019-02-02 20:41:03 +0530

    Text fields

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 
-class CreateProduct extends StatelessWidget {
+class CreateProduct extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return CreateProductState();
+  }
+}
+
+class CreateProductState extends State<CreateProduct>{
+  
+  String name;
+  double price; 
+  String desc;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement buildll;
-    return Center(
-      child:RaisedButton(
-        child: Text("Create Product"),
-        onPressed: (){
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context){
-              return Center(child: Text("Hello there"),);
-            });
-        },));
-  }
+    return Column(
+      children: <Widget>[
+        TextField(onChanged: (String i){
+            // setState(() {
+             this.name = i;
+            //});
+        },),
+        TextField(onChanged: (String i){
+          this.price = double.parse(i);
+        },
+          keyboardType: TextInputType.number,
+        ),
+        TextField(
+          keyboardType: TextInputType.multiline,
+          maxLines: 5,
+          onChanged: (String i) => this.desc = i
+        ),
+      ],
+    );
+  } 
+  
 }
+
