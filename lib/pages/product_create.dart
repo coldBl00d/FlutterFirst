63e5bf1de5b5f55109fdccc14a61b6047743ecd5
akhraj <akhilrajr28@gmail.com>
2019-02-06 23:25:36 +0530

    cleaner product_create page

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -17,6 +17,59 @@ class CreateProductState extends State<CreateProduct> {
   double _price;
   String _desc;
 
+  Widget _buildTitleTF() {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: 'Title',
+        //icon: Icon(Icons.edit)
+      ),
+      onChanged: (String i) {
+        // setState(() {
+        this._name = i;
+        //});
+      },
+    );
+  }
+
+  Widget _buildPriceTF() {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: 'Price',
+        //icon: Icon(Icons.edit)
+      ),
+      onChanged: (String i) {
+        this._price = double.parse(i);
+      },
+      keyboardType: TextInputType.number,
+    );
+  }
+
+  Widget _buildDescTF() {
+    return TextField(
+        decoration: InputDecoration(
+          labelText: 'Description',
+          //icon: Icon(Icons.edit)
+        ),
+        keyboardType: TextInputType.multiline,
+        maxLines: 5,
+        onChanged: (String i) => this._desc = i);
+  }
+
+  void _submitForm() {
+    final Map<String, dynamic> product = {
+      'title': this._name,
+      'desc': this._desc,
+      'price': this._price,
+      'image': 'assets/food.jpg'
+    };
+
+    widget._addProduct(product);
+    Navigator.pushReplacementNamed(context, '/products');
+
+    //pass data to main dart
+    //widget._addProduct()
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement buildll;
@@ -24,35 +77,9 @@ class CreateProductState extends State<CreateProduct> {
         margin: EdgeInsets.all(20.0),
         child: ListView(
           children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Title',
-                //icon: Icon(Icons.edit)
-              ),
-              onChanged: (String i) {
-                // setState(() {
-                this._name = i;
-                //});
-              },
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Price',
-                //icon: Icon(Icons.edit)
-              ),
-              onChanged: (String i) {
-                this._price = double.parse(i);
-              },
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-                decoration: InputDecoration(
-                  labelText: 'Description',
-                  //icon: Icon(Icons.edit)
-                ),
-                keyboardType: TextInputType.multiline,
-                maxLines: 5,
-                onChanged: (String i) => this._desc = i),
+            this._buildTitleTF(),
+            this._buildPriceTF(),
+            this._buildDescTF(),
             SizedBox(
               height: 20.0,
             ),
@@ -61,20 +88,7 @@ class CreateProductState extends State<CreateProduct> {
               child: Text(
                 "Save",
               ),
-              onPressed: () {
-                final Map<String, dynamic> product = {
-                  'title': this._name,
-                  'desc': this._desc,
-                  'price': this._price,
-                  'image': 'assets/food.jpg'
-                };
-
-                widget._addProduct(product);
-                Navigator.pushReplacementNamed(context, '/products');
-
-                //pass data to main dart
-                //widget._addProduct()
-              },
+              onPressed: this._submitForm,
               color: Theme.of(context).accentColor,
             )
           ],
