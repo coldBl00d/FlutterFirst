20a4f7e32c0c1d2311174e292638a659a42140a4
Akhil Raj <akhraj@oradev.oraclecorp.com>
2019-02-23 15:20:54 +0530

    Better product list - avatar, subtitl and divider

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -14,23 +14,31 @@ class ListProduct extends StatelessWidget {
       padding: EdgeInsets.all(10),
       child: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Container(
-              height: 40,
-              width: 100,
-              child: Image.asset(this._products[index]['image']),
-            ),
-            title: Text(this._products[index]['title']),
-            trailing: IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return EditProduct(product: this._products[index], updateProduct: this._updateProduct, index: index,);
+          return Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(this._products[index]['image']),
+                ),
+                title: Text(this._products[index]['title']),
+                subtitle: Text('\$${this._products[index]['price']}'),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return EditProduct(
+                          product: this._products[index],
+                          updateProduct: this._updateProduct,
+                          index: index,
+                        );
+                      },
+                    ));
                   },
-                ));
-              },
-            ),
+                ),
+              ),
+              Divider(),
+            ],
           );
         },
         itemCount: this._products.length,
